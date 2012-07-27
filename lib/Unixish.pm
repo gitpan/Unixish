e8406068090008edf89bb4eef2847df78112ab8d
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-27 14:46:50 +0000

    Import of SHARYANTO/Unixish-1.0.2 from CPAN.
    gitpan-cpan-distribution: Unixish
    gitpan-cpan-version:      1.0.2
    gitpan-cpan-path:         SHARYANTO/Unixish-1.0.2.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Unixish.pm
+++ lib/Unixish.pm
@@ -1,6 +1,6 @@
 package Unixish;
 
-our $VERSION = '1.0.1'; # VERSION
+our $VERSION = '1.0.2'; # VERSION
 
 1;
 # ABSTRACT: Data transformation framework, inspired by Unix toolbox philosophy
@@ -14,7 +14,7 @@ Unixish - Data transformation framework, inspired by Unix toolbox philosophy
 
 =head1 VERSION
 
-version 1.0.1
+version 1.0.2
 
 =head1 AUTHOR
 
